6208522d63f89eddf19f5f0b68259179e93560b4
Gavin <xie_youjiang@163.com>
2016-11-14 06:57:48 -0800

    模板类

---
+++ algorihm.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include "algorihm.h"
+template<class T>
+Algorihm<T>::Algorihm()
+{
+	
+}
+
+template<class T>
+Algorihm<T>::~Algorihm()
+{
+	
+}
+
+template<class T>
+void Algorihm<T>::fun(const T[], T &target )
+{
+	
+}
+int main()
+{
+	return 0;
+}
---
+++ algorihm.h
@@ -0,0 +1,9 @@
+template<class T>
+class Algorihm
+{
+public:
+	Algorihm();
+	~Algorihm();
+public:
+	void fun(const T[], T &target);
+};
