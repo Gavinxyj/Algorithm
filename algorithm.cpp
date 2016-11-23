bb7c32e4818eb9ad62e3b733e63e888704181fc8
Gavin <xie_youjiang@163.com>
2016-11-23 05:21:36 -0800

    新增直接插入排序，折半插入排序

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include "algorithm.h"
 
+
 template<class T>
 Algorithm<T>::Algorithm()
 {
@@ -93,4 +94,61 @@ void Algorithm<T>::quickSort(T *array, std::size_t nLow, std::size_t nHigh, int 
 		array[nFirst] = nKey;
 		quickSort(array, nLow, nFirst - 1, fCompare);//对小于key的左边区域再排序
 		quickSort(array, nFirst + 1, nHigh, fCompare);//对大于key的区域再排序
+}
+
+template<class T>
+void Algorithm<T>::insertSort(T *array, std::size_t nLength, int (*fCompare)(const T*, const T*))
+{
+		if(0 == *array || nLength <= 0) return;
+			
+		T temp;
+		int index = 0;
+		
+		for(int nLoop = 1; nLoop < nLength; nLoop ++)
+		{
+			temp = array[nLoop];
+			
+			for(index = nLoop; index > 0 && fCompare(&array[index - 1], &temp) > 0; index --)
+			{
+				array[index] = array[index -1];	
+			
+			}	
+			array[index] = temp;
+			
+		}
+}
+
+template<class T>
+void Algorithm<T>::binaryInsertSort(T *array, std::size_t nLength, int (*fCompare)(const T*, const T*))
+{
+	 if(0 == *array || nLength <= 0) return;
+	 	
+	 
+	 for(int nLoop = 1; nLoop < nLength; nLoop ++)
+	 {
+	 		T temp = array[nLoop];
+	 		
+	 		int nLow  = 0;
+	 		int nHigh = nLoop - 1;
+	 		
+	 		while(nLow <= nHigh)
+	 		{
+	 			int nMid = (nLow + nHigh) / 2;
+	 			
+	 			if(fCompare(&temp, &array[nMid]) > 0)
+	 			{
+	 				nLow = nMid + 1;	
+	 			}	
+	 			else
+	 			{
+	 				nHigh = nMid - 1;
+	 			}
+	 		}
+	 		
+	 		for(int index = nLoop; index > nLow; index --)
+	 		{
+	 			array[index] = array[index - 1]; 			
+	 		}
+	 		array[nLow] = temp;	
+	 }
 }
--- algorithm.h
+++ algorithm.h
@@ -13,7 +13,11 @@ public:
 	void selectionSort(T *array, std::size_t length, int (*fCompare)(const T *, const T *));
 		
 	void quickSort(T *array, std::size_t nFirst, std::size_t nLast, int (*fCompare)(const T *, const T *));
+	
+	void insertSort(T *array, std::size_t nLength, int (*fCompare)(const T*, const T*));
 
+	void binaryInsertSort(T *array, std::size_t nLength, int (*fCompare)(const T*, const T*));
+		
 };
 
 #endif
